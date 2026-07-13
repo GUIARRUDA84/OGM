e5c40da5484464ea2be5cf655f2e2ccf021d0c15
GUIARRUDA84 <142699129+GUIARRUDA84@users.noreply.github.com>
2026-07-13 15:55:30 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+/* Cole aqui a URL da implantação do Apps Script (termina em /exec). */
+window.API_URL = 'https://script.google.com/macros/s/AKfycbw35qeg7EgowMxZdjFtgCEDz8mSyFf4tpsx5nvwLWPIYIpF5QqaL1HQho8MH2n0qNIqDw/exec';
+
+/* Identificação do sistema (aparece no cabeçalho). */
+window.APP_TITULO = 'Pronto Atendimento';
+window.APP_SUBTITULO = 'Ouvidoria-Geral do Município de Londrina';
